00300b7a2375222f20abef70c67eca5286f5a8ea
Bahodir Abdusaxatov <88647942+Bahodir22@users.noreply.github.com>
2022-10-10 21:05:25 +0500

    upd#6
    ...

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        if(salary<=10000){
+        if(salary>=0 && salary<=10000){
             System.out.println(salary-salary*0.15);
         }
         else if (10000<salary && salary<=20000) {
@@ -11,7 +11,7 @@ public class SalaryCalculator {
         else if (salary>20000) {
             System.out.println(salary-salary*0.2);
         }
-        else {
+        else{
             System.out.println("wrong input!");
         }
     }
